9d041d45982a68faa418dd62c77b39a39fa9768e
s106631@ap.be <kasper.bosteels@student.ap.be>
2021-10-28 16:01:52 +0200

    gatsby plugins

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,16 @@
 //stap 1 importeer react
 import * as React from "react"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 //step defineer je component
 const aboutPage = () => {
   return (
     <Layout pageTitle="about-us">
       <p>lorem ispum teskt teskskdzpakdopkzadk</p>
+      <StaticImage
+        alt="een diep zee cnidarian"
+        src="../images/deep_sea_cnidarian.jpg"
+      />
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 // stap 1 import react
 import * as React from "react"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 //stap 2 definieer je component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Welcome to Bosteel Agency.">
       <p>lorem ipsum</p>
+      <StaticImage
+        alt="randomized unsplash image!"
+        src="https://cdn.mos.cms.futurecdn.net/cw6tn8mDAproPTjdjqndFV.jpg"
+      />
     </Layout>
   )
 }
